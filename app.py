cce9166fce35a808b5a913878b062a72d82c02e8
Justin Schroeder <justin.schroeder3@gmail.com>
2021-03-09 18:41:56 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+"""
+Meets Expectations:
+1. Create New Script called app.py - Done
+2. Use Dunder Main to prevent execution on import
+3. Import PLAYERS from constants.py
+4. Create clean_data function
+5. Create balance_teams function
+6. Ensure Console Readability
+7. Display Team stats
+
+Exceeds:
+1. Clean gaurdians in clean_data
+2. Balance by player exp in balance_teams
+3. Include additional stats
+4. Quit Menu Option
+
+"""
+
+import constants
+
+if __name__ == "__main__":
+    
